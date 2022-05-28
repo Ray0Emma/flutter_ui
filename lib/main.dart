ce239c9a42acf0627f84809cfcd12670bd87d7ad
Ray0Emma <ahmadifarah22@gmail.com>
2022-05-28 09:07:53 +0000

    feat: added the pink paragraph

--- lib/main.dart
+++ lib/main.dart
@@ -43,143 +43,157 @@ class _MyHomePageState extends State<MyHomePage> {
     // by the _incrementCounter method above.
 
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 13),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Top Stories',
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Samedi 20 Mai',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Top Stories',
+                        style: TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Samedi 20 Mai',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Stack(children: [
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.transparent,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.asset('assets/user_flutter.jpg'),
                       ),
                     ),
-                  ],
+                    Positioned(
+                      right: 4,
+                      top: 1,
+                      child: Container(
+                        width: 10,
+                        height: 10,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 14.0,
+                              style: BorderStyle.solid),
+                        ),
+                      ),
+                    )
+                  ]),
+                ],
+              ),
+            ),
+            SizedBox(height: 12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset('assets/flutter_ntr.jpg', fit: BoxFit.fill),
+                Padding(
+                  padding: EdgeInsets.only(left: 13, top: 13, bottom: 15),
+                  child: Text(
+                    'Addiction When Gambling Becomes A Problem',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
                 ),
-                Stack(children: [
+              ],
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 13, top: 10, bottom: 15),
+              child: Row(
+                children: [
                   CircleAvatar(
-                    radius: 25,
+                    radius: 19,
                     backgroundColor: Colors.transparent,
                     child: ClipRRect(
-                      borderRadius: BorderRadius.circular(25),
+                      borderRadius: BorderRadius.circular(19),
                       child: Image.asset('assets/user_flutter.jpg'),
                     ),
                   ),
-                  Positioned(
-                    right: 4,
-                    top: 1,
-                    child: Container(
-                      width: 10,
-                      height: 10,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                            color: Colors.white,
-                            width: 14.0,
-                            style: BorderStyle.solid),
-                      ),
-                    ),
-                  )
-                ]),
-              ],
-            ),
-          ),
-          SizedBox(height: 12),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset('assets/flutter_ntr.jpg', fit: BoxFit.fill),
-              Padding(
-                padding: EdgeInsets.only(left: 13, top: 13, bottom: 15),
-                child: Text(
-                  'Addiction When Gambling Becomes A Problem',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 13, top: 10, bottom: 15),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 19,
-                  backgroundColor: Colors.transparent,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(19),
-                    child: Image.asset('assets/user_flutter.jpg'),
+                  SizedBox(
+                    width: 13,
                   ),
-                ),
-                SizedBox(
-                  width: 13,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Farah Ait Elahmadi',
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    Text(
-                      'durée',
-                      style: TextStyle(color: Colors.grey),
-                    )
-                  ],
-                ),
-              ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Farah Ait Elahmadi',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      Text(
+                        'durée',
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 11,
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 13, right: 13, bottom: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in."),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const <Widget>[
-                    Icon(
-                      Icons.favorite_rounded,
-                      color: Colors.pink,
-                      size: 24.0,
-                      semanticLabel: 'Pink Heart Icon',
-                    ),
-                    SizedBox(width: 10), // give it width
-                    Text('1125'),
-                    SizedBox(width: 30), // give it width
-                    Icon(
-                      Icons.comment_outlined,
-                      color: Colors.pink,
-                      size: 24.0,
-                    ),
-                    SizedBox(width: 10), // give it width
-                    Text('348'),
-                  ],
-                )
-              ],
+            SizedBox(
+              height: 11,
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.only(left: 13, right: 13, bottom: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in."),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.favorite_rounded,
+                        color: Colors.pink,
+                        size: 24.0,
+                        semanticLabel: 'Pink Heart Icon',
+                      ),
+                      SizedBox(width: 10), // give it width
+                      Text('1125',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(width: 30), // give it width
+                      Icon(
+                        Icons.comment_outlined,
+                        color: Colors.grey[300],
+                        size: 24.0,
+                      ),
+                      SizedBox(width: 10), // give it width
+                      Text(
+                        '348',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.pink,
+              padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+              child: Text(
+                " To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. ",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
